925c5a850b0891119ce4dd640031aaf9190d6b9e
Jose <pastorpedraza3@gmail.com>
2021-07-01 11:34:47 -0500

    Add user routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,10 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :update, :destroy]
+  before_action :set_user, only: [:login, :destroy]
+  
 
-  # GET /users
-  def index
-    @users = User.all
-
-    render json: @users
-  end
-
-  # GET /users/1
-  def show
-    render json: @user
+  # GET /users/login/:id
+  def login
+    render json: UserRepresenter.new(@user).as_json, status: :ok
   end
 
   # POST /users
@@ -19,16 +13,7 @@ class UsersController < ApplicationController
     @user.password = params[:password]
 
     if @user.save
-      render json: @user, status: :created, location: @user
-    else
-      render json: @user.errors, status: :unprocessable_entity
-    end
-  end
-
-  # PATCH/PUT /users/1
-  def update
-    if @user.update(user_params)
-      render json: @user
+      render json: UserRepresenter.new(@user).as_json, status: :created
     else
       render json: @user.errors, status: :unprocessable_entity
     end
